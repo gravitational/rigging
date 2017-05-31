890c188121f706f0c44bcf2825469812c2baa524
dmitri <deemok@gmail.com>
2017-05-31 15:11:37 +0200

    Report error on rig status for reverted changeset

--- changeset.go
+++ changeset.go
@@ -154,6 +154,8 @@ func (cs *Changeset) Status(ctx context.Context, changesetNamespace, changesetNa
 	case ChangesetStatusCommitted:
 		// Nothing to do
 		return nil
+	case ChangesetStatusReverted:
+		return trace.BadParameter("changeset reverted")
 	}
 
 	if retryAttempts == 0 {
