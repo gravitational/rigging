ed16b51ddc371183eaf61743d6bca797970633d2
Kevin Nisbet <kevin@gravitational.com>
2021-01-14 15:30:16 -0500

    [5.5] backport 86,87 (#94)
    * Re-entrancy on rigging + upgrade tweaks (#86)
    
    * adjust rigging to be more re-entrant / inline with how customers try and use gravity/rigging
    
    * address review feedback
    
    (cherry picked from commit f933ef9d8a6c8786905755d274361d06d7d60ab7)
    
    * Don't revert operations that are already reverted (#87)
    
    
    (cherry picked from commit 84363ae1d1c7e1065d06530e4319c75df2beb301)

--- utils.go
+++ utils.go
@@ -402,5 +402,5 @@ func waitForObjectDeletion(fn func() error) error {
 
 const (
 	deletePollInterval = 1 * time.Second
-	deleteTimeout      = 5 * time.Minute
+	deleteTimeout      = 10 * time.Minute
 )
