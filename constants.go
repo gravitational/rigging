bc158e784924ab36ea9de1539f80b4df2c9473e3
Sergei Antipov <greendayonfire@gmail.com>
2021-08-22 09:13:52 -0400

    Update prometheus-operator to v0.49.x

--- constants.go
+++ constants.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
